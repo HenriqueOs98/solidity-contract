408c4a9534f5845a388be09540e4ffcfb022267e
Henrique <henriqueos98@outlook.com>
2025-05-01 13:04:53 -0300

    adding everything

---
+++ src/Counter.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract Counter {
+    uint256 private number;
+
+    function getNumber() view public returns(uint256) {
+        return number;
+    }
+
+    function setNumber(uint256 newNumber) public {
+        number = newNumber;
+    }
+
+    function increment() public {
+        number++;
+    }
+}
